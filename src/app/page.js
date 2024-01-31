ad9d2402366e06ef1a1459af4f6d1e4d22339ae7
Abeer958 <84776419+AbeerWebDev@users.noreply.github.com>
2024-01-31 12:38:03 +0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,30 @@
+import styles from './home.module.css'
+import Image from 'next/image';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency.</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit..</p>
+        <div className={styles.buttons}>
+          <button className={styles.buttons}>Learn More</button>
+          <button className={styles.buttons}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="https://images.pexels.com/photos/13417078/pexels-photo-13417078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.png" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
