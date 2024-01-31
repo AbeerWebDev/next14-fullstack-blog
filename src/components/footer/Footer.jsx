7ca91b7ee748d3d7a40f7ffb8d98e941ae343d80
Abeer958 <84776419+AbeerWebDev@users.noreply.github.com>
2024-01-31 17:29:33 +0400

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.logo}>AbeerWebDev</div>
+      <div className={styles.text}>
+       creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 }
 
 export default Footer
